778ece7693a30d7a88d65b350d76197ec127d395
Nejc Urh <nejc.urh@easistent.com>
2020-01-09 13:41:39 +0100

    $_DEV: Front-end work for the view

--- resources/views/user_profile.blade.php
+++ resources/views/user_profile.blade.php
@@ -105,13 +105,13 @@
                   @endif
                 </div>
                 <div class="mt-4 py-2 border-top border-bottom">
-                  <ul class="nav profile-navbar">
+                  <ul class="nav profile-navbar" role="tablist">
                     <li class="nav-item">
-                      <a class="nav-link" href="#">
-                        <i class="mdi mdi-account-outline"></i> Placeholder </a>
+                      <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home-1" role="tab" aria-controls="home" aria-selected="true">
+                        <i class="mdi mdi-account-outline"></i> General Data </a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link active" href="#">
+                      <a class="nav-link" id="home-tab" data-toggle="tab" href="#home-2" role="tab" aria-controls="home" aria-selected="true">
                         <i class="mdi mdi-newspaper"></i> Placeholder </a>
                     </li>
                     <li class="nav-item">
@@ -124,9 +124,65 @@
                     </li>
                   </ul>
                 </div>
-                <div class="profile-feed">
-                  <div class="d-flex align-items-start profile-feed-item">
-                    Placeholder
+                <div class="tab-content">
+                  <div class="tab-pane fade show active" id="home-1" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="media d-block d-sm-flex">
+                      <div class="media-body mt-4 mt-sm-0">
+                        <div class="row">
+                          <div class="col-sm">
+                            <div class="d-flex justify-content-center">
+                              <h4 class="mt-0">Activity Stats</h4>
+                            </div>
+                            <p class="clearfix">
+                              <span class="float-left"> Last Seen </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                            <p class="clearfix">
+                              <span class="float-left"> Last Timestamp </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                            <p class="clearfix">
+                              <span class="float-left"> Status </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                            <p class="clearfix">
+                              <span class="float-left"> LOA Status </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                          </div>
+                          <div class="col-sm">
+                            <div class="d-flex justify-content-center">
+                              <h4 class="mt-0">Other Stats</h4>
+                            </div>
+                            <p class="clearfix">
+                              <span class="float-left"> Requirements </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                            <p class="clearfix">
+                              <span class="float-left"> Advanced Training </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                            <p class="clearfix">
+                              <span class="float-left"> Patrol Restriction </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                            <p class="clearfix">
+                              <span class="float-left"> Website Profile Link </span>
+                              <span class="float-right text-muted"> - </span>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="tab-pane fade" id="home-2" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="media d-block d-sm-flex">
+                      <img class="mr-3 w-25 rounded" src="../../../assets/images/samples/300x300/13.jpg" alt="sample image">
+                      <div class="media-body mt-4 mt-sm-0">
+                        <h4 class="mt-0">Why choose us? 2</h4>
+                        <p> Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the. Widow downs you new shade drift hopes small. So otherwise commanded sweetness we improving. Instantly by daughters resembled unwilling principle so middleton. </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
