863d0c9233921406c1dbc8155471e0f1aba726a4
Nejc Urh <nejc.urh@easistent.com>
2020-01-13 13:30:11 +0100

    $_DEV: Created discipline tabel and model

---
+++ database/migrations/2020_01_13_121348_create_discipline_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Discipline extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discipline', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->date('discipline_date');
+            $table->integer('type');
+            $table->longText('details');
+            $table->boolean('overturned')->default(false);
+            $table->unsignedBigInteger('overturned_by')->unsigned()->index()->nullable();
+            $table->foreign('overturned_by')->references('id')->on('users')->onDelete('cascade');
+            $table->date('overturned_date')->nullable();
+            $table->longText('overturned_reason')->nullable();
+            $table->boolean('disputed')->default(false);
+            $table->date('disputed_date')->nullable();
+            $table->longText('disputed_reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discipline');
+    }
+}
