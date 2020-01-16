cb20eae84b97acd67836a62cf841662ee0ff5fa1
Nejc Urh <nejc.urh@easistent.com>
2020-01-16 14:53:02 +0100

    $_DEV: Created git branch system

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
+        Bluora\LaravelGitInfo\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Datatables' => Yajra\Datatables\Datatables::class,
+        'Git' => Bluora\LaravelGitInfo\Facade::class,
 
     ],
 
