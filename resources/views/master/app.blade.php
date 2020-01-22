20527f56b6dc3a731340fa67dde11d9ea7fb657e
Nejc Urh <nejc.urh@easistent.com>
2020-01-22 19:55:43 +0100

    $_DEV: New javascript directory

--- resources/views/master/app.blade.php
+++ resources/views/master/app.blade.php
@@ -3,16 +3,19 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ $constants['global']['application_name'] }} :: {{ $constants['pages'][$page_name] }}</title>
+
     <!-- plugins:css -->
     <link rel="stylesheet" href="/assets/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.base.css">
     <!-- endinject -->
-    <!-- Plugin CSS -->
+
+    <!-- Global Plugin CSS -->
     <link rel="stylesheet" href="/assets/vendors/jvectormap/jquery-jvectormap.css">
     <link rel="stylesheet" href="/assets/vendors/flag-icon-css/css/flag-icon.min.css">
     <link rel="stylesheet" href="/assets/vendors/owl-carousel-2/owl.carousel.min.css">
@@ -23,41 +26,57 @@
     <link rel="stylesheet" href="/assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
     <link rel="stylesheet" href="/assets/vendors/jquery-toast-plugin/jquery.toast.min.css">
     <link rel="stylesheet" href="/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+
     <!-- Plugin css for this page -->
     @yield('customcss')
-    <!-- End plugin css for this page -->
-    <!-- End plugin css -->
     <!-- inject:css -->
     <link href="/assets/vendors/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <!-- endinject -->
+
     <!-- Layout styles -->
     <link rel="stylesheet" href="/assets/css/xcs-int/custom.css">
     <link rel="stylesheet" href="/assets/css/modern-vertical/style.css">
     <!-- End layout styles -->
+
+    <!-- XCS-Int Favicon -->
     <link rel="shortcut icon" href="/assets/images/xcs-int/{{ $constants['global']['application_favicon'] }}" />
+    <!-- endfavicon -->
+
   </head>
+
+<!-- WEB APPLICATION BEGIN -->
   <body>
+
     <div class="container-scroller">
+
       <!-- partial:master/sidebar.blade.php -->
       @include('master.sidebar')
-      <!-- partial -->
+      <!-- endpartial -->
+
       <div class="container-fluid page-body-wrapper">
+
         <!-- partial:master/header.blade.php -->
         @include('master.header')
-        <!-- partial -->
+        <!-- endpartial -->
+
         <div class="main-panel">
+
           <!-- content-wrapper begins -->
           @yield('content')
           <!-- content-wrapper ends -->
+
           <!-- partial:master/footer.blade.php -->
           @include('master.footer')
-          <!-- partial -->
+          <!-- endpartial -->
+
         </div>
         <!-- main-panel ends -->
+
       </div>
       <!-- page-body-wrapper ends -->
+
     </div>
-    <!-- container-scroller -->
+
     <!-- plugins:js -->
     <script src="/assets/vendors/js/vendor.bundle.base.js"></script>
     <script src="/assets/vendors/moment/moment.min.js"></script>
@@ -67,79 +86,37 @@
     <script src="/assets/vendors/jquery-toast-plugin/jquery.toast.min.js"></script>
     <script src="/assets/vendors/datatables.net/jquery.dataTables.js"></script>
     <script src="/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
     @yield('pluginjs')
-    <!-- End plugin js for this page -->
+    <!-- endinject -->
+
     <!-- inject:js -->
     <script src="/assets/js/off-canvas.js"></script>
     <script src="/assets/js/hoverable-collapse.js"></script>
     <script src="/assets/js/misc.js"></script>
     <script src="/assets/js/settings.js"></script>
     <script src="/assets/js/todolist.js"></script>
+    @yield('injectjs')
     <!-- endinject -->
+
     <!-- Custom js for this page -->
     @yield('customjs')
     <!-- End custom js for this page -->
+
     <!-- Ajax for this page -->
-    <script type="text/javascript">
-      $.ajaxSetup({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-
-    globalToast = function(heading, text, icon, color) {
-      'use strict';
-      $.toast({
-        heading: heading,
-        text: text,
-        showHideTransition: 'slide',
-        icon: icon,
-        loaderBg: color,
-        position: 'top-right'
-      })
-    };
-  (function($) {
-    'use strict';
-
-    if ($(".js-example-basic-single").length) {
-      $(".js-example-basic-single").select2();
-    }
-    if ($(".js-example-basic-multiple").length) {
-      $(".js-example-basic-multiple").select2();
-    }
-  })(jQuery);
-    </script>
     @yield('ajax')
     <!-- End ajax for this page -->
+
     <!-- Global modals for this page-->
     @include('modals.submit_patrol_log_modal')
     <!-- End global modals for this page-->
-    <!-- End modals for this page -->
+
     <!-- Modals for this page-->
     @yield('modals')
     <!-- End modals for this page -->
 
-    <script type="text/javascript">
-    if ($(".ajax_search_member-class").length) {
-      $(".ajax_search_member-class").select2();
-    }
-    $(".ajax_search_member-class").select2({
-        placeholder: "Search by Website ID, Name or Unit Number",
-        allowClear: true
-    });
-    $("#ajax_search_member-click_redirect").on("select2:select", function (e) {
-      window.location.replace(e.params.data.id);
-    });
-
-    if ($(".antelope_global_select_single").length) {
-      $(".antelope_global_select_single").select2();
-    }
-    $(".antelope_global_select_single").select2({
-        placeholder: "Select an Option...",
-        allowClear: true
-    });
-    </script>
+    <!-- XCS-Int Javascript-->
+    <script src="/js/app.js"></script>
+    <!-- End XCS-Int Javascript-->
   </body>
 </html>
+<!-- WEB APPLICATION END -->
