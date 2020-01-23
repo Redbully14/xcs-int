78160034b95cdd366d2f858b185d4347162f809a
Nejc Urh <nejc.urh@easistent.com>
2020-01-23 13:43:28 +0100

    $_DEV: Finished working on javascript libraries

--- resources/views/modals/show_patrol_log_modal.blade.php
+++ resources/views/modals/show_patrol_log_modal.blade.php
@@ -65,44 +65,7 @@
 </div>
 
 <script type="text/javascript">
-    $table = $('#tableElement');
-    $table.on('click', '#ajax_view_patrol_log_open', function () {
-
-    var id = $(this).val();
-
-      $.ajax({
-         type: "POST",
-         url: '{{ url('activity/get_data/') }}/'+id,
-         success: function(data){
-           console.log(data);
-            // $("#profile-username-field").val(data['username']);
-            if(data['department_id'] == null) {
-                var uniqueunitnumber = data['user_name'];
-             } else var uniqueunitnumber = data['user_name']+' '+data['department_id'];
-            $("#ajax_view_patrol_log").modal("toggle");
-            $("#ajax-patrol-log-by").empty();
-            $("#ajax-patrol-log-by").append(uniqueunitnumber);
-            $("#ajax-patrol-website-id").empty();
-            $("#ajax-patrol-website-id").append(data['website_id']);
-            $("#ajax-input-patrol-log-type").val(data['type']).change();
-            $("#ajax-input-patrol-log-start-date").val(data['patrol_start_date']);
-            $("#ajax-input-patrol-log-end-date").val(data['patrol_end_date']);
-            $("#ajax-input-patrol-start-time").val(data['start_time']);
-            $("#ajax-input-patrol-end-time").val(data['end_time']);
-            $("#ajax-input-patrol-details").val(data['details']);
-         }
-      }).done(function(data) {});
-  });
-
-  (function($) {
-    'use strict';
-
-    if ($(".js-example-basic-single").length) {
-      $(".js-example-basic-single").select2();
-    }
-    if ($(".js-example-basic-multiple").length) {
-      $(".js-example-basic-multiple").select2();
-    }
-  })(jQuery);
+  var $url_show_patrol_log = '{{ url('activity/get_data/') }}/';
 </script>
+<script src="js/modals/show_patrol_log_modal.js"></script>
 @endif
