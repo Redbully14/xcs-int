3447632651ea099ca06f61dab50becfec94636c1
Nejc Urh <nejc.urh@easistent.com>
2020-01-28 13:18:47 +0100

    $_DEV: Established migration and Absence modal

---
+++ database/migrations/2020_01_28_081347_create_absence_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsenceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absence', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('forum_post');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absence');
+    }
+}
