5d3d8c60aca19b2d89ad6d1693e53c9647f547f5
Nejc Urh <nejc.urh@easistent.com>
2020-02-10 13:41:45 +0100

    $_DEV: Better tooltips

--- public/js/app.js
+++ public/js/app.js
@@ -27,6 +27,40 @@ globalToast = function(heading, text, icon, color) {
   }
 })(jQuery);
 
+(function($) {
+  'use strict';
+
+  $(function() {
+    /* Code for attribute data-custom-class for adding custom class to tooltip */
+    if (typeof $.fn.tooltip.Constructor === 'undefined') {
+      throw new Error('Bootstrap Tooltip must be included first!');
+    }
+
+    var Tooltip = $.fn.tooltip.Constructor;
+
+    // add customClass option to Bootstrap Tooltip
+    $.extend(Tooltip.Default, {
+      customClass: ''
+    });
+
+    var _show = Tooltip.prototype.show;
+
+    Tooltip.prototype.show = function() {
+
+      // invoke parent method
+      _show.apply(this, Array.prototype.slice.apply(arguments));
+
+      if (this.config.customClass) {
+        var tip = this.getTipElement();
+        $(tip).addClass(this.config.customClass);
+      }
+
+    };
+    $('[data-toggle="tooltip"]').tooltip();
+
+  });
+})(jQuery);
+
 $(".ajax_search_member-class").select2({
     placeholder: "Search by Website ID, Name or Unit Number",
     allowClear: true
