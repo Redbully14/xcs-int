fb95c1f5752c770e45befa7a9d535c7a9c8dd488
Christopher <mrcm8263@gmail.com>
2020-02-13 19:25:32 +1100

    $_DEV: Sexy api comments

--- routes/api.php
+++ routes/api.php
@@ -13,18 +13,74 @@
 | https://antelope.docs.apiary.io/
 */
 
+/**
+ * Webdomain: /api/
+ *
+ * @author Christopher M.
+ * @package POST
+ * @access Public
+ * @version 1.0.0
+ */
 Route::post('/', function () {
     return (new \App\Http\Resources\ApiError(['message' => 'No Api version provided.']))->response()->setStatusCode(400);
 });
 
+/*
+|--------------------------------------------------------------------------
+| Route Group: api
+| Category Name: v1ApiRoutes
+|--------------------------------------------------------------------------
+|
+*/
+
 Route::prefix('v1')->namespace('API')->group(function () {
+    /**
+     * Webdomain: /api/v1/
+     *
+     * @author Christopher M.
+     * @package POST
+     * @category v1ApiRoutes
+     * @access Public
+     * @version 1.0.0
+     */
+
     Route::post('/', function () {
         return (new \App\Http\Resources\ApiError(['message' => 'No endpoint provided.']))->response()->setStatusCode(400);
     });
 
+    /**
+     * Webdomain: /api/v1/{any}/{any}/{any}
+     *
+     * @author Christopher M.
+     * @package POST
+     * @category v1ApiRoutes
+     * @access Api Token
+     * @version 1.0.0
+     */
+
     Route::post('/{endpoint}/{argument}/{argument2}', 'ApiEndpointController@interpret')->middleware('throttle');
 
+    /**
+     * Webdomain: /api/v1/{any}/{any}
+     *
+     * @author Christopher M.
+     * @package POST
+     * @category v1ApiRoutes
+     * @access Api Token
+     * @version 1.0.0
+     */
+
     Route::post('/{endpoint}/{argument}', 'ApiEndpointController@interpret')->middleware('throttle');
+
+    /**
+     * Webdomain: /api/v1/{any}
+     *
+     * @author Christopher M.
+     * @package POST
+     * @category v1ApiRoutes
+     * @access Api Token
+     * @version 1.0.0
+     */
 
     Route::post('/{endpoint}', 'ApiEndpointController@interpret')->middleware('throttle');
 });
