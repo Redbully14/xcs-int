d4bf25f4ef88c0167c21aee90cdeab506a1e3d8f
Nejc Urh <nejc.urh@easistent.com>
2020-02-17 14:32:49 +0100

    $_DEV: Minor site improvements

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
   <body>
     <div class="container-scroller">
       <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="row w-100">
+        <div class="row w-100" style="margin-left: 0px">
           <div class="content-wrapper full-page-wrapper auth login-2 login-bg"  style="background: url('{{$constants['backgrounds']['login']}}'); background-size: cover;">
             <div class="card col-lg-4">
               <div class="card-body px-5 py-5">
