27d88e6f566ff0a1cd4b75e709e2c54900d4f135
Nejc Urh <nejc.urh@easistent.com>
2020-02-28 14:24:03 +0100

    $_DEV: Warn of unsaved changes

--- public/js/internal_roster.js
+++ public/js/internal_roster.js
@@ -177,3 +177,20 @@ function internalRoster_changeWebsiteid(data) {
 function internalRoster_changeRank(data) {
   $(data).addClass('internal_roster-changed_rank');
 };
+
+var formSubmitting = false;
+var setFormSubmitting = function() { formSubmitting = true; };
+
+window.onload = function() {
+    window.addEventListener("beforeunload", function (e) {
+        if (formSubmitting) {
+            return undefined;
+        }
+
+        var confirmationMessage = 'It looks like you have been editing something. '
+                                + 'If you leave before saving, your changes will be lost.';
+
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
+};
--- resources/views/internal_roster.blade.php
+++ resources/views/internal_roster.blade.php
@@ -31,242 +31,243 @@ table {
 	</div>
 	<br>
 	<br>
-
-	<div class="row">
-		<h2>Department Chain of Command</h2>
-	</div>
-	<div class="row">
-		<h3>Department Administration</h3>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-admins" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  <form id="internal_roster-form" onsubmit="setFormSubmitting()">
+  	<div class="row">
+  		<h2>Department Chain of Command</h2>
+  	</div>
+  	<div class="row">
+  		<h3>Department Administration</h3>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-admins" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<div class="row">
-		<h3>Department Senior Staff</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-seniorstaff" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<div class="row">
+  		<h3>Department Senior Staff</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-seniorstaff" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<div class="row">
-		<h3>Department Staff</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-staff" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<div class="row">
+  		<h3>Department Staff</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-staff" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<div class="row">
-		<h3>Department Staff in Training</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-staffintraining" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<div class="row">
+  		<h3>Department Staff in Training</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-staffintraining" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<br>
-	<div class="row">
-		<h2>Department Members</h2>
-	</div>
-	<div class="row">
-		<h3>Senior Members</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-seniormembers" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<br>
+  	<div class="row">
+  		<h2>Department Members</h2>
+  	</div>
+  	<div class="row">
+  		<h3>Senior Members</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-seniormembers" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<div class="row">
-		<h3>Members</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-members" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<div class="row">
+  		<h3>Members</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-members" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<div class="row">
-		<h3>Probationary Members</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-probationarymembers" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<div class="row">
+  		<h3>Probationary Members</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-probationarymembers" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<br>
-	<div class="row">
-		<h2>Department Reserves</h2>
-	</div>
-	<div class="row">
-		<h3>Reserves</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-reserves" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<br>
+  	<div class="row">
+  		<h2>Department Reserves</h2>
+  	</div>
+  	<div class="row">
+  		<h3>Reserves</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-reserves" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div><hr>
+  	</div><hr>
 
-	<div class="row">
-		<h3>Media</h1>
-	</div>
-	<div class="row">
-		<div class="col-12">
-          <div class="table-responsive">
-            <table id="internal_roster-media" class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Unit Number</th>
-                  <th>Website ID</th>
-                  <th>Rank</th>
-                  <th>Status</th>
-                  <th>Monthly Hours</th>
-                  <th>Monthly Logs</th>
-                  <th>Advanced Training</th>
-                  <th>Requirements</th>
-                </tr>
-              </thead>
-            </table>
+  	<div class="row">
+  		<h3>Media</h1>
+  	</div>
+  	<div class="row">
+  		<div class="col-12">
+            <div class="table-responsive">
+              <table id="internal_roster-media" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Unit Number</th>
+                    <th>Website ID</th>
+                    <th>Rank</th>
+                    <th>Status</th>
+                    <th>Monthly Hours</th>
+                    <th>Monthly Logs</th>
+                    <th>Advanced Training</th>
+                    <th>Requirements</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
           </div>
-        </div>
-	</div>
+  	</div>
+  </form>
 </div>
 @endsection
 
